6d547570d8cc6ca66234f52698bb97ad654746d7
honglucorporation <27552403@qq.com>
2014-11-16 23:31:58 +0800

    回退代码

--- cloud/main.js
+++ cloud/main.js
@@ -2,95 +2,12 @@
 // For example:
 var name = require('cloud/name.js');
 require('cloud/app.js')
-//var myutils = require('cloud/utils.js');
-//var common = require('cloud/common.js');
 
 AV.Cloud.define("hello", function(request, response) {
 	response.success("Hello world," + request.params.userid);
 });
 
-/**
- * 获取融云token接口
- * @userobjid   用户objectid，通过该ID获取到用户信息，再向融云发起获取token请求
- */
-/*
-AV.Cloud.define('getimtoken', function(req, res){
-	var ret = {
-		status:"success"
-	};
 
-	myutils.printObject();
-	var dumpOutput = myutils.dumpObj(req,'request','1', 3);
-	console.log(dumpOutput);
-
-	//获取用户id
-	var userobjid = req.params.userid;
-	if (userobjid ==  undefined) {
-		res.error('userid is expected!');
-		return;
-	}
-
-	console.log("userid:%s", userobjid);
-
-	//根据id查询用户表
-	var hpUser = AV.Object.extend("_User");
-	var query = new AV.Query(hpUser);
-	query.get(userobjid, {
-		success:function(userObj) {
-			var username = userObj.get('nickname');
-			var icon = userObj.get('icon');
-
-			//融云校验信息
-			var appSecret = common.rongCloudAppSecret; // 开发者平台分配的 App Secret。
-			var nonce = Math.floor(Math.random()*100000); // 获取随机数。
-			var nowTime = new Date();
-			var timestamp = Math.floor(nowTime/1000); // 获取时间戳。
-
-			var sourcedata = appSecret + nonce.toString() + timestamp.toString();
-			var signature = myutils.SHA1(sourcedata); //生成签名
-
-			console.log("nonce:%d timestamp:%d singature:%s source:%s", nonce, timestamp, signature, sourcedata);
-
-			//通过avcloud发送HTTP的post请求
-			AV.Cloud.httpRequest({
-				method: 'POST',
-				url: 'https://api.cn.rong.io/user/getToken.json',
-				headers: {
-					'App-Key': common.rongCloudAppKey,
-					'Nonce': nonce,
-					'Timestamp': timestamp,
-					'Signature': signature
-				},
-				body: {
-					userId:userobjid,
-					name:username,
-					portraitUri:icon
-				},
-				success: function(httpResponse) {
-					console.log(myutils.dumpObj(httpResponse, 'httpresponse', 'mark', 5));
-					console.log(httpResponse.text);
-
-					res.success(httpResponse.data);
-				},
-				error: function(httpResponse) {
-					var errmsg = 'Request failed with response code ' + httpResponse.status;
-					console.log(errmsg);
-					res.error(errmsg);
-				}
-			});
-		},
-		error:function(object,error) {
-			// The object was not retrieved successfully.
-			// error is a AV.Error with an error code and description.
-			console.log(error);
-
-			var errmsg = 'query object fail:' + error.code;
-
-			res.error(errmsg);
-		}
-	});
-});
-*/
 AV.Cloud.beforeSave("TestReview", function(request, response){
 	if (request.object.get("stars") < 1) {
 		response.error("you cannot give less than one star");
