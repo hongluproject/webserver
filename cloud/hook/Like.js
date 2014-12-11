474190027d9a796401a27d8bb8b11c93b0e5dbef
garyfu <garyfu@pptv.com>
2014-12-11 20:16:59 +0800

    解决点赞失败的问题。

--- cloud/hook/Like.js
+++ cloud/hook/Like.js
@@ -8,15 +8,31 @@
  */
 AV.Cloud.afterSave('Like', function(request){
     var likeType = request.object.get('like_type');
+    var targetId = request.object.get('external_id');
     if (likeType == 1) {	//资讯点赞
-        var newsObj = request.object.get('newsid');
-        newsObj.increment('up_count');
-        newsObj.save();
-
+        var query = new AV.Query('News');
+        query.get(targetId, {
+            success: function(result) {
+                console.info("Like afterSave up_count increment for News,current up_count is %d", result.get('up_count'));
+                result.increment("up_count");
+                result.save();
+            },
+            error: function(error) {
+                console.error( "Like afterSave:Got an error " + error.code + " : " + error.message);
+            }
+        });
     } else if (likeType == 2) {	//动态点赞
-        var dynamicObj = request.object.get('dynamic_id');
-        dynamicObj.increment('up_count');
-        dynamicObj.save();
+        var query = new AV.Query('DynamicNews');
+        query.get(targetId, {
+            success: function(result) {
+                console.info("Like afterSave up_count increment for DynamicNews,current up_count is %d", result.get('up_count'));
+                result.increment('up_count');
+                result.save();
+            },
+            error: function(error) {
+                console.error( "Like afterSave:Got an error " + error.code + " : " + error.message);
+            }
+        });
     }
 });
 
@@ -25,22 +41,41 @@ AV.Cloud.afterSave('Like', function(request){
  */
 AV.Cloud.afterDelete('Like', function(request) {
     var likeType = request.object.get('like_type');
+    var targetId = request.object.get('external_id');
     if (likeType == 1) {	//资讯点赞
-        var newsObj = request.object.get('newsid');
-        if (newsObj.get('up_count') > 0) {
-            newsObj.increment('up_count', -1);
-            newsObj.save();
-        } else {
-            console.warn('afterDelete Like for news:up_count is less than zero');
-        }
-
+        var query = new AV.Query('News');
+        query.get(targetId, {
+            success: function(result) {
+                console.info("Like afterDelete up_count increment for news,current up_count is %d", result.get('up_count'));
+                if (result.get('up_count') > 0) {
+                    result.increment("up_count", -1);
+                    result.save();
+                } else {
+                    console.info('up_count is less than zero');
+                }
+            },
+            error: function(error) {
+                console.error( "Like afterSave:Got an error " + error.code + " : " + error.message);
+            }
+        });
     } else if (likeType == 2) {	//动态点赞
-        var dynamicObj = request.object.get('dynamic_id')
-        if (dynamicObj.get('up_count') > 0) {
-            dynamicObj.increment('up_count', -1);
-            dynamicObj.save();
-        } else {
-            console.warn('afterDelete Like for dynamic:up_count is less than zero');
-        }
+        var query = new AV.Query('DynamicNews');
+        query.get(targetId, {
+            success: function(result) {
+                console.info("Like afterDelete up_count increment for DynamicNews,current up_count is %d", result.get('up_count'));
+                //点赞次数累加
+                if (result.get('up_count') > 0) {
+                    result.increment('up_count', -1);
+                    result.save();
+                } else {
+                    console.info('up_count is less than zero');
+                }
+
+                //发送消息流给动态发布者
+            },
+            error: function(error) {
+                console.error( "Like afterSave:Got an error " + error.code + " : " + error.message);
+            }
+        });
     }
 });
