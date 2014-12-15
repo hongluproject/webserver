c6b178ba4071023e2bdcff282b3b114de5ea67ae
garyfu <garyfu@pptv.com>
2014-12-15 19:25:20 +0800

    动态增加返回tagNames字段，不用APP在本地转换。

--- cloud/function/dynamicWrapper.js
+++ cloud/function/dynamicWrapper.js
@@ -33,17 +33,24 @@ AV.Cloud.define('getDynamic', function(req,res){
                 icon:1,
                 __type:1
             };
+            var date1, date2, date3;
+
+            console.info("limit:%d maxId:%d", limit, maxId);
 
             //查询事件流，获取用户关注的所有动态
             var query = AV.Status.inboxQuery(AV.User.createWithoutData('_User',userId));
             query.include('dynamicNews');
-            query.include('source');
+//            query.include('source');
             query.include('dynamicNews.user_id');
             query.equalTo('messageType', 'newPost');
             query.limit(limit);
             query.maxId(maxId);
             query.exists('dynamicNews');
+            date1 = new Date();
             query.find().then(function(statuses){
+                date2 = new Date();
+                console.info("dynamic finding use time:%d ms", date2.getTime()-date1.getTime());
+
                 //获取所有动态objectId，再查询该用户对这些动态是否点过赞
                 var dynamicIdArray = [];
                 for (var i in statuses) {
@@ -64,11 +71,14 @@ AV.Cloud.define('getDynamic', function(req,res){
                 console.dir(err);
                 res.error('查询关注动态信息失败！');
             }).then(function(likes){
+                date3 = new Date();
+                console.info("dynamic like finding use time:%d ms", date3.getTime()-date2.getTime());
                 for (var i in likes) {
                     likeTarget[likes[i].get('external_id')] = true;
                 }
 
 
+                var hpTags = AV.HPGlobalParam.hpTags;
                 //将所有动态返回，添加isLike，记录点赞状态
                 for (var i in statusesReturn) {
                     var currDynamic = statusesReturn[i].data.dynamicNews;
@@ -76,6 +86,17 @@ AV.Cloud.define('getDynamic', function(req,res){
                         currDynamic.isLike = true;
                     else
                         currDynamic.isLike = false;
+
+                    //从tagId转换tagName，并返回给APP
+                    if (hpTags) {
+                        var arrayTagName = [];
+                        for (var k in currDynamic.tags) {
+                            arrayTagName.push(hpTags[currDynamic.tags[k]].get('tag_name') || '');
+                        }
+                        if (arrayTagName.length) {
+                            currDynamic.tagNames = arrayTagName;
+                        }
+                    }
 
                     //遍历user_id，去掉不需要返回的字段，减少网络传输
                     for (var k in currDynamic.user_id) {
