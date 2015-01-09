0a88858e065d593893ec30d488e0402e933b0143
suaxu <43046435@qq.com>
2015-01-09 16:39:58 +0800

    搜索部落优化

--- cloud/function/clan.js
+++ cloud/function/clan.js
@@ -5,6 +5,7 @@ AV.Cloud.define("getClan",function(req, res){
     var index = Math.floor((Math.random()*tags.length));
     var User = AV.Object.extend("_User");
     var Clan = AV.Object.extend("Clan");
+    var userInfo = null;
     var ret = {
         selfClan:{},
         recommendClan:{}
@@ -14,8 +15,9 @@ AV.Cloud.define("getClan",function(req, res){
 
     var getSelfClan = function(userid){
         var query = new AV.Query(User);
-        query.select("nickname","tags","clanids");
+        query.select("nickname","tags","clanids","actual_position");
         query.get(userid).then(function(result) {
+            userInfo =  result;
             var  clanids = result.get("clanids");
             if(clanids){
                 var query = new AV.Query(Clan);
@@ -56,36 +58,56 @@ AV.Cloud.define("getClan",function(req, res){
     }
 
     var getRecommendClan = function (clanids){
+        var userGeoPoint = userInfo.get('actual_position');
         var query = new AV.Query(Clan);
         query.limit(2);
         if(clanids)
         query.notContainedIn("objectId",clanids);
         if(tags[index])
         query.equalTo("tags", tags[index]);
+        if (userGeoPoint)
+        query.near("position", userGeoPoint);
         query.find({
             success: function(result) {
-                var recommendClan = [];
-                for (var i = 0; i < result.length; i++) {
-                    var outResult = {};
-                    var arrayTagName = [];
-                    var arrayTag = result[i].get('tags');
-                    for (var k in arrayTag) {
-                        var name = '';
-                        if (HPGlobalParam.hpTags[arrayTag[k]]) {
-                            name = HPGlobalParam.hpTags[arrayTag[k]].get('tag_name');
-                        }
-                        arrayTagName.push(name);
-                    }
-                    if (arrayTagName.length) {
-                        result[i].set('tagsName', arrayTagName);
-                    }
-                    outResult       = result[i];
-                    recommendClan.push(outResult);
+                if(result.length==0){
+                    var query = new AV.Query(Clan);
+                    query.limit(2);
+                    if(clanids)
+                        query.notContainedIn("objectId",clanids);
+                    if (userGeoPoint)
+                        query.near("position", userGeoPoint);
+                        query.find({
+                            success : function(result){
+                                formatResult(result);
+                            }
+                        })
+                }else{
+                    formatResult(result);
                 }
-                ret.recommendClan = recommendClan;
-                res.success(ret);
             }
         });
+        var formatResult =function (result){
+            var recommendClan = [];
+            for (var i = 0; i < result.length; i++) {
+                var outResult = {};
+                var arrayTagName = [];
+                var arrayTag = result[i].get('tags');
+                for (var k in arrayTag) {
+                    var name = '';
+                    if (HPGlobalParam.hpTags[arrayTag[k]]) {
+                        name = HPGlobalParam.hpTags[arrayTag[k]].get('tag_name');
+                    }
+                    arrayTagName.push(name);
+                }
+                if (arrayTagName.length) {
+                    result[i].set('tagsName', arrayTagName);
+                }
+                outResult       = result[i];
+                recommendClan.push(outResult);
+            }
+            ret.recommendClan = recommendClan;
+            res.success(ret);
+        }
     }
     getSelfClan(userid);
 });
