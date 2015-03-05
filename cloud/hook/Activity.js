375912a57b90008a3319c579a790f1765488c01b
suaxu <43046435@qq.com>
2015-03-05 17:35:13 +0800

    Revert "活动 。活动报名云函数"
    This reverts commit 00107aae8744aa455d93888dc1222906289b270c.

--- cloud/hook/Activity.js
+++ cloud/hook/Activity.js
@@ -6,22 +6,5 @@ AV.Cloud.afterSave('Activity', function(request) {
     ActivityObj.set('share_url', 'https://hoopeng.avosapps.com/activity/' + ActivityId);
     ActivityObj.save();
 
-    var query = new AV.Query('Goods');
-    query.equalTo('activityId', AV.Object.createWithoutData('Activity', ActivityId));
-    query.count({
-        success: function(count){
-            if(count == 0){
-                var Goods = AV.Object.extend('Goods');
-                var goods = new Goods();
-                goods.set('activityId', AV.Object.createWithoutData('Activity', ActivityId));
-                goods.save();
-            }
-            return;
-         },
-        error: function(err){
-            console.info("save goods error "+ err);
-        }
-    });
-
 
 });
