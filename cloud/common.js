d069bc61910e933b11b3ac4d43fc42a724a648d3
suaxu <43046435@qq.com>
2015-05-05 15:53:02 +0800

    活动开始结束时间

--- cloud/common.js
+++ cloud/common.js
@@ -22,7 +22,6 @@ Date.prototype.Format = function(fmt)
         "q+" : Math.floor((this.getMonth()+3)/3), //季度
         "S"  : this.getMilliseconds()             //毫秒
     };
-    console.info(11111111123);return;
     if(/(y+)/.test(fmt))
         fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
     for(var k in o)
